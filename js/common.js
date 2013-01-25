31c79921ac0d4bbde2b82a8071be29cdcef92231
mirantel <magi.mirantel@gmail.com>
2013-01-25 14:30:53 +0200

    product

--- js/common.js
+++ js/common.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-
+	$('.product .slides').cycle({ 
+		fx:     'scrollHorz', 
+		prev:   '.slider .slider__prev', 
+		next:   '.slider .slider__next', 
+		timeout: 0,
+		api:true,
+		before: function(curr){
+			$(this).parents('.product').find('.product__nav li').eq($(this).index()).addClass('active').siblings().removeClass('active');
+		}
+	});
+	$('.product .product__nav li').click(function(){
+		if (!($(this).hasClass('active'))) {
+			asd = $(this).index();
+			$('.product .slides').cycle(asd);
+		};
+		return false;
+	});
 });
